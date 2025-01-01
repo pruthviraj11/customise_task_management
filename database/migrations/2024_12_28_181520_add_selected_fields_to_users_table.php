5c9fa628856c0fbd728a46b0dd82eb35db161c79
Yash <yrabadia99@gmail.com>
2025-01-01 11:45:40 +0530

    chnanges in migration file

--- database/migrations/2024_12_28_181520_add_selected_fields_to_users_table.php
+++ database/migrations/2024_12_28_181520_add_selected_fields_to_users_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->json('selected_fields')->nullable();
+            $table->json('selected_fields')->default(json_encode([]));
+
         });
     }
 
@@ -27,7 +28,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('selected_fields');
+            //
         });
     }
 };
