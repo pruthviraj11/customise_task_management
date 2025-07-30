4da8e9ba47bf7ca0943f041c14a5e5fc506a5cac
Yash <yrabadia99@gmail.com>
2025-07-30 18:47:15 +0530

    changes

--- resources/views/content/apps/dashboard/customized_index.blade.php
+++ resources/views/content/apps/dashboard/customized_index.blade.php
@@ -823,7 +823,8 @@
                                         var match = data.match(/>([^<]+)</);
                                         return match ? match[1] : data;
                                     }
-                                    return data;
+                                    // return data;
+                                    return $('<div>').html(data).text();
                                 }
                             }
                         },
@@ -1198,7 +1199,8 @@
                                         var match = data.match(/>([^<]+)</);
                                         return match ? match[1] : data;
                                     }
-                                    return data;
+                                    // return data;
+                                    return $('<div>').html(data).text();
                                 }
                             }
                         },
