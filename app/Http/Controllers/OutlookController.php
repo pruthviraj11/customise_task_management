10a6641daac358c8c11b311a4ac91b764d4c66f3
Yash <yrabadia99@gmail.com>
2025-08-04 10:34:19 +0530

    changes

--- app/Http/Controllers/OutlookController.php
+++ app/Http/Controllers/OutlookController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Auth;
 
+
 class OutlookController extends Controller
 {
     public function redirectToMicrosoft()
@@ -100,8 +101,8 @@ class OutlookController extends Controller
                 'outlook_token_expires' => now()->addSeconds($data['expires_in'] ?? 3600),
             ]);
 
- $taskController = new TaskController();
-    $result = $taskController->outlook_sync();
+$taskController = app(TaskController::class);
+$taskController->outlook_sync();
             return redirect()->route('app-task-list')->with('success', 'Outlook connected successfully!');
         } else {
             \Log::error('Outlook OAuth Token Error', [
